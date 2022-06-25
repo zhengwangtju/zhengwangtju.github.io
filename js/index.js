aaa1dbbd727c8ef8efb44a94a9e0116a1eaf2c44
ght <573955230@qq.com>
2022-06-25 14:07:15 +0800

    fanhui

--- js/index.js
+++ js/index.js
@@ -1,16 +1,40 @@
-$(document).ready(function(){
-    var now = "tab1";
-    $(".tab").click(function(){
-        var index = $(this).attr("name");
-        if(index!=now){
-            $("."+now).removeClass("line");
-            $("."+index).addClass("line");
+// $(document).ready(function(){
+//     var now = "tab1";
+//     $(".tab").click(function(){
+//         var index = $(this).attr("name");
+//         if(index!=now){
+//             console.log(now);
+//             console.log(index);
+//             $("."+now).removeClass("line");
+//             $("."+index).addClass("line");
 
             
-            $("."+now + "-content").addClass("none");
-            $("."+index + "-content").removeClass("none");
+//             $("."+now + "-content").addClass("none");
+//             $("."+index + "-content").removeClass("none");
             
-            now = index;
-        }
-    })
-});
+//             now = index;
+//         }
+//     })
+// });
+ 
+// $(function(){
+//     $(".tab5").click(function() {
+        
+//         window.location.href="project.html";
+//         // console.log(this);
+//         // setTimeout(function () {
+//         //     console.log("11111");
+//         //     // $(".tab1").removeClass("line");
+//         //     // console.log("2222222");
+//         //     // $(".tab5").addClass("line");
+//         //     // $(".tab1-content").addClass("none");
+//         //     // console.log("1111111111");
+//         //     // $(".tab5-content").removeClass("none");
+//         //     // console.log("button返回修改被点击了")
+//         // },1000);
+
+        
+       
+//     })
+    
+// } )
